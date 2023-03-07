ba45c52d0205103d9e701bc483f4e916627d3f92
ryantoxx <114425150+ryantoxx@users.noreply.github.com>
2023-03-07 09:48:41 +0200

    add lab2

--- src/Lab1 - Regular grammars/main.py
+++ src/Lab1 - Regular grammars/main.py
@@ -1,7 +1,8 @@
 from grammar import Grammar
 from finiteAutomaton import finiteAutomata
 
-# V-10
+'''
+LAB - 1
 v_n = ['S','B','L']
 v_t = ['a','b', 'c']
 p = {
@@ -25,3 +26,29 @@ if new_automaton.if_valid(test_string):
     print('Correct')
 else:
     print('Incorrect')
+'''
+
+# LAB - 2
+
+VN = {'S', 'B', 'L'}
+VT = {'a', 'b', 'c'}
+P = {
+    'S': {'aB'},
+    'B': {'bB', 'cL'},
+    'L': {'cL', 'aS', 'b'}
+}
+
+grammar = Grammar(VN, VT, P)
+grammar_classification = grammar.chomsky_hierarchy()
+print(grammar_classification)
+
+Q = {'q0', 'q1', 'q2', 'q3'}
+Sigma = {'a', 'b', 'c'}
+F = {'q3'}
+delta = {
+    ('q0', 'a'): {'q1'},
+    ('q1', 'b'): {'q2', 'q1'},
+    ('q2', 'c'): {'q3'},
+    ('q3', 'a'): {'q1'},
+    ('q0', 'b'): {'q2'}
+}
