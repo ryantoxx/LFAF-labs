3967a810d4489390d83aa2f0909fa4cd6a1fd1dc
ryantoxx <114425150+ryantoxx@users.noreply.github.com>
2023-03-11 17:02:03 +0200

    update

--- src/Lab1 - Regular grammars/main.py
+++ src/Lab1 - Regular grammars/main.py
@@ -29,26 +29,50 @@ else:
 '''
 
 # LAB - 2
+Regular_Grammar = Grammar('S', ['S', 'B', 'L'], ['a', 'b', 'c'],
+                        { 
+                            'S': ['aB'],
+                            'B': ['bB', 'cL'],
+                            'L': ['cL', 'aS', 'b']
+                        }, {'a'})
 
-VN = {'S', 'B', 'L'}
-VT = {'a', 'b', 'c'}
-P = {
-    'S': {'aB'},
-    'B': {'bB', 'cL'},
-    'L': {'cL', 'aS', 'b'}
-}
-
-grammar = Grammar(VN, VT, P)
-grammar_classification = grammar.chomsky_hierarchy()
-print(grammar_classification)
-
+gr_type = Regular_Grammar.chomsky_hierarchy()
+print('---------------------------')
+print(gr_type)
+print('---------------------------')
+'''
 Q = {'q0', 'q1', 'q2', 'q3'}
 Sigma = {'a', 'b', 'c'}
 F = {'q3'}
 delta = {
     ('q0', 'a'): {'q1'},
-    ('q1', 'b'): {'q2', 'q1'},
+    ('q1', 'b'): {'q2'},
     ('q2', 'c'): {'q3'},
     ('q3', 'a'): {'q1'},
+    ('q1', 'b'): {'q1'},
     ('q0', 'b'): {'q2'}
 }
+'''
+Finite_Automaton = finiteAutomata(['q0', 'q1', 'q2', 'q3'], ['a', 'b', 'c'], 'q0',
+                                  [ ('q0', 'a', 'q1'),
+                                    ('q1', 'b', 'q2'),
+                                    ('q2', 'c', 'q3'),
+                                    ('q3', 'a', 'q1'),
+                                    ('q1', 'b', 'q1'),
+                                    ('q0', 'b', 'q2')                                    
+                                  ],
+                                   {'q3'})
+
+gr = Finite_Automaton.fa_to_rg()
+print('q0 ->' , gr.S)
+print('VN ->', gr.VN)
+print('VT ->', gr.VT)
+print('P ->', gr.P)
+
+print('---------------------------')
+
+print("The fa is Deterministic" if Finite_Automaton.fa_det() else "The fa is Non Deterministic")
+
+print('---------------------------')
+
+
