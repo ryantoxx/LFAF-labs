3372659f53e38690f26de7833e8c4dca46fa1cdf
ryantoxx <114425150+ryantoxx@users.noreply.github.com>
2023-04-17 17:01:21 +0300

    update main file

--- src/Lab1 - Regular grammars/main.py
+++ src/Lab1 - Regular grammars/main.py
@@ -1,10 +1,31 @@
 import lexer
+from grammar import Grammar
+from finiteAutomaton import finiteAutomata
+from CNF import CNF
 
-while True:
-    text = input('lexer > ')
-    tokens, error = lexer.run('<example.txt',text)
 
-    if error: 
-        print(error.as_string())
-    else: 
-        print(tokens)
+VN = ['S', 'A', 'B', 'D']
+VT = ['a', 'b', 'd']
+P = {'S': ['dB', 'AB'],
+     'A': ['d', 'dS', 'aAaAb', ''],
+     'B': ['a', 'aS', 'A'],
+     'D': ['Aba']
+}
+S = 'S'
+
+grammar = CNF(VN, VT, P, S)
+
+print('\nCFG productions:\n')
+print('P =', P)
+
+print('\n1st Step: Eliminate null productions:')
+grammar.null_production()
+
+print('\n2nd Step: Eliminate unit productions:')
+grammar.unit_production()
+
+print('\n3rd Step: Eliminate useless symbols:')
+grammar.eliminate_useless_symbols()
+
+print('\nFinal form:')
+grammar.toNormalForm()
